d31f4a8dd44bc98af1c88343f993a2fec64f17e2
Desta <destaanugrapratama@gmail.com>
2025-05-27 06:13:21 +0700

    update

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use('/api/votes', voteRoutes);
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Export app untuk Vercel
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+if (require.main === module) {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+}
 
 module.exports = app;
